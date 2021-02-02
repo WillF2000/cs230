ba704a378f5aae584bbc6fa8015dbaae57f26963
WillF2000 <waf0006@mix.wvu.edu>
2021-02-02 16:29:51 -0500

    Lab 2

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1 style="color: violet">About Us</h1>
+        <p>Computer Science 230</p>
+    </div>
+    <div class="row">
+        <div class= "column">
+            <div class="card">
+                <img src="images/4.jpg">
+                <div class="container">
+                    <h2>Boss</h2>
+                    <p style="color: red">Michael Scott</p>
+                    <p>Email: whatSheSaid@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class= "column">
+            <div class="card">
+                <img src="images/3.jpg">
+                <div class="container">
+                    <h2>The Muscle</h2>
+                    <p style="color: red">Arnold Schwarzenegger</p>
+                    <p>Email: mrOlympia@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class= "column">
+            <div class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>Jokester 1</h2>
+                    <p style="color: red">Will Ferrell</p>
+                    <p>Email: BrennanHuff123@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class= "column">
+            <div class="card">
+                <img src="images/2.jpg">
+                <div class="container">
+                    <h2>Jokester 2</h2>
+                    <p style="color: red">Peter Griffin</p>
+                    <p>Email: PGabc@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
