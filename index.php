99a02002bd4cce4dc866bd7f2aaa253dd60112dd
WillF2000 <waf0006@mix.wvu.edu>
2021-02-04 16:52:26 -0500

    About us detail added

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 
     <div class="about">
         <h1 style="color: violet">About Us</h1>
-        <p>Computer Science 230</p>
+        <p>The Dream Team
+        <p>The team destined for greatness! Led by Michael Scott, from Dunder Mifflin, the team is in the best hands possible. The team built of muscle and jokes will have no problem defeating those who stand in their way while competing on America's Got Talent. It will take blood sweat and tears as these gladiators go into battle!
     </div>
     <div class="row">
         <div class= "column">
             <div class="card">
                 <img src="images/4.jpg">
                 <div class="container">
-                    <h2>Boss</h2>
+                    <h2>The Boss</h2>
                     <p style="color: red">Michael Scott</p>
                     <p>Email: whatSheSaid@cs230.com</p>
                 </div>
